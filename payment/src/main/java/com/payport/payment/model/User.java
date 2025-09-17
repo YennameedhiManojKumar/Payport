8dba1e6acd27319933da527939c43dc5bd406310
Manoj <manojkumar944140@gmail.com>
2025-09-18 01:57:23 +0530

    Fix: re-added payment folder as normal directory

---
+++ payment/src/main/java/com/payport/payment/model/User.java
@@ -0,0 +1,42 @@
+package com.payport.payment.model;
+
+import jakarta.persistence.*;
+
+@Entity
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String mobileNumber;
+
+    // getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+}
